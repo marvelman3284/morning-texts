dbaead76b23f7d783536651480e8e3af526d7f2d
Jeevan Shah <jedimaster3284@gmail.com>
2024-02-24 13:06:27 -0500

    updates

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import requests
 
 
 def text() -> None:
-    '''Get the needed information and send a text message'''
+    """Get the needed information and send a text message"""
 
     # Read and load the config file
-    with open('config.yml', 'r') as f:
+    with open("config.yml", "r") as f:
         conf = yaml.safe_load(f)
 
     # Get the calender events for today and the next day
@@ -16,18 +16,24 @@ def text() -> None:
     events = "\n".join(events)
 
     # Get todays weather
-    week_weather = weather.get_weather(conf['API']['key'], conf['API']['lat'], conf['API']['lon'])
+    week_weather = weather.get_weather(
+        conf["API"]["key"], conf["API"]["lat"], conf["API"]["lon"]
+    )
     week_weather = week_weather[0]
 
     # Combine all the information into one string
     message = f"Upcoming you have:\n{events}.\nToday's weather is: \n{week_weather}"
 
-    # Send the text message using textbelt    
-    resp = requests.post('https://textbelt.com/text', {
-        'phone': conf['Message']['number'],
-        'message': message,
-        'key': 'textbelt',
-    })
+    # Send the text message using textbelt
+    resp = requests.post(
+        "https://textbelt.com/text",
+        {
+            "phone": conf["Message"]["number"],
+            "message": message,
+            "key": "textbelt",
+        },
+    )
     print(resp.json())
+
 
 text()
